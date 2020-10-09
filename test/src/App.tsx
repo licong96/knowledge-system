416333c9fac5722a69546ed3b3ec372d71664900
cassli <cassli@vipabc.com>
2020-10-09 09:46:09 +0800

    test

---
+++ test/src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
